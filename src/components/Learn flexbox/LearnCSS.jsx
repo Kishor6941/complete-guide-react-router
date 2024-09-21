d65031fbbcaea23096f0c57b71083156bb7e1fa7
Kishor6941 <phawadeks9049@gmail.com>
2024-09-21 12:10:17 +0530

    flex, flex-direction, flex-wrap,flex-flow

--- src/components/Learn flexbox/LearnCSS.jsx
+++ src/components/Learn flexbox/LearnCSS.jsx
@@ -5,7 +5,16 @@ const LearnCSS = () => {
         <div className="main-container">
             <div className="one">1</div>
             <div className="two">2</div>
-            <div className="three">3</div>
+            <div className="three">4</div>
+            <div className="one">5</div>
+            <div className="two">6</div>
+            <div className="three">7</div>
+            <div className="one">8</div>
+            <div className="two">9</div>
+            <div className="three">10</div>
+            <div className="one">11</div>
+            <div className="two">12</div>
+            <div className="three">13</div>
         </div>
         </>
     )
